2c6c1f27355af198cb4cd384928702d94c26a488
Marc Santiago <marcanthonysanti@gmail.com>
2024-01-28 17:11:06 -0500

    V3: Migration

--- background.js
+++ background.js
@@ -1,11 +1,6 @@
-let state = false;
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  if (!state) {
-    chrome.tabs.insertCSS(null, { file: "dark_mode.css" });
-    state = !state;
-    return;
-  }
-  chrome.tabs.insertCSS(null, { file: "light_mode.css" });
-  state = !state;
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['content.js']
+  });
 });
